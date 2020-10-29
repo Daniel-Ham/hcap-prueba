f752d1f2f53f79144e2f7d34b47857408aedb78e
Daniel-Ham <dahamva@DESKTOP-74E0AGA.localdomain>
2020-10-29 13:32:28 -0600

    agregar nuevas funcionalidades y archivos

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,5 +1,5 @@
 import cv2
-
+import numpy as np
 imagen=cv2.imread('imagen.jpg')
 imagen= cv2.cvtColor(imagen,cv2.COLOR_BGR2RGB)
 print(imagen.shape)
@@ -12,11 +12,18 @@ imagen= cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
 print(imagen.shape)
 print(imagen[0][0])
 
-imagen[0][0]=0;
-imagen[0][1]=0;
-imagen[0][2]=0;
+imagen[0][0]=0
+imagen[0][1]=0
+imagen[0][2]=0
 
 cv2.imwrite('grayimage.jpg',imagen)
+
+matriz=np.zeros((256,256),np.float32)
+print(matriz.shape)
+cv2.imwrite('matrizimagen.jpg',matriz)
+imagen=cv2.cvtColor(matriz,cv2.COLOR_GRAY2BGR)
+print(imagen.shape)
+cv2.imwrite('matrizcolorimagen.jpg',imagen)
 
 
 #only native linux and mac
